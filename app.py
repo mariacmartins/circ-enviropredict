c1a2e57f64bfaae4bae6a94ebc533851c046bb20
Maria Clara Martins <52721823+mariacmartins@users.noreply.github.com>
2024-10-17 04:08:55 -0300

    add cold model

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Load models
-w2v_model = Word2Vec.load("models/word2vec_model_maize.bin")
+w2v_model_maize = Word2Vec.load("models/word2vec_model_maize.bin")
+w2v_model_rice = Word2Vec.load("models/word2vec_model_rice.bin")
+
 drought_model = load('models/rf_model_drought.joblib')
+cold_model = load('models/rf_model_cold.joblib')
 
 # Params
 kmer_size = 3
@@ -24,32 +27,33 @@ def circrna_to_kmers(circrna_sequence, k=kmer_size):
         kmers.append(kmer)
     return kmers
 
-def circrna_to_vec(circrna_sequence, k=kmer_size):
+def circrna_to_vec(circrna_sequence, model, k=kmer_size):
     vec = np.zeros(vector_size)
     kmers = circrna_to_kmers(circrna_sequence, k=k)
     for kmer in kmers:
-        if kmer in w2v_model.wv:
-            vec = vec + w2v_model.wv[kmer]
+        if kmer in model.wv:
+            vec = vec + model.wv[kmer]
     return vec
 
 def is_valid_sequence(sequence):
     sequence = sequence.replace(" ", "").replace("\n", "")
-
     return len(sequence) >= 20 and re.fullmatch(r'[acgtunACGTUN]+', sequence) is not None
 
 # User-Streamlit interface
 col1, col2 = st.columns([3, 1])  
 
 with col1:
-  st.title('circ-EnviroPredict')
-    
+    st.title('circ-EnviroPredict')
+
 with col2:
-  st.image('logo.png', width=80)  
+    st.image('logo.png', width=80)  
 
-st.subheader('A Machine Learning tool that allows the prediction of the possible involvement of circRNAs with abiotic stress.')
-st.write('*For now, we only have predictive models to evaluate the possible involvement of circRNAs in drought stress. But in the future, we intend to add more conditions.*')
+st.markdown('##### A Machine Learning tool that allows the prediction of the possible involvement of circRNAs with abiotic stress.')
 
+# Selecting the type of stress
+stress_type = st.radio("Select the type of stress:", ('drought', 'cold'))
 
+# Input seq and Submit
 seq_input = st.text_area("**circRNA sequence:**", height=150, placeholder="Example of accepted format: CTCGGGCACCTCCTCCGAGACCACTGAT. At least 20 characters are required.")
 
 if st.button('Submit'):
@@ -57,20 +61,36 @@ if st.button('Submit'):
         # Transforming input data to Word2vec format
         columns = [f'wc_3mer_{v+1}' for v in range(vector_size)]
         df_vecs = pd.DataFrame(columns=columns)
+        
+        if stress_type == 'drought':
+            model_to_use = w2v_model_maize
+        if stress_type == 'cold':  
+            model_to_use = w2v_model_rice
+
         df_vecs = df_vecs._append(
-            [dict(zip(columns, circrna_to_vec(seq_input)))],
+            [dict(zip(columns, circrna_to_vec(seq_input, model_to_use)))],
             ignore_index=True
         )
         final_df = pd.concat([df_vecs], axis=1)
 
-        # Prediction
-        y_pred = drought_model.predict(final_df)
-        y_pred_proba = drought_model.predict_proba(final_df)
+        # Prediction based on selected stress type
+        if stress_type == 'drought':
+            y_pred = drought_model.predict(final_df)
+            y_pred_proba = drought_model.predict_proba(final_df)
 
-        if y_pred[0] == 1:
-            st.write(f'**Prediction result:** Possible involvement with drought stress. Probability: {y_pred_proba[:, 1]}.')
-        else:
-            st.write(f'**Prediction result:** No involvement with drought stress detected. Probability: {y_pred_proba[:, 0]}.')
+            if y_pred[0] == 1:
+                st.write(f'**Prediction result:** Possible involvement with drought stress. Probability: {y_pred_proba[:, 1][0]:.2f}.')
+            else:
+                st.write(f'**Prediction result:** No involvement with drought stress detected. Probability: {y_pred_proba[:, 0][0]:.2f}.')
+
+        elif stress_type == 'cold':
+            y_pred = cold_model.predict(final_df)
+            y_pred_proba = cold_model.predict_proba(final_df)
+
+            if y_pred[0] == 1:
+                st.write(f'**Prediction result:** Possible involvement with cold stress. Probability: {y_pred_proba[:, 1][0]:.2f}.')
+            else:
+                st.write(f'**Prediction result:** No involvement with cold stress detected. Probability: {y_pred_proba[:, 0][0]:.2f}.')
 
     else:
         st.write('Please enter a valid circRNA sequence.')
